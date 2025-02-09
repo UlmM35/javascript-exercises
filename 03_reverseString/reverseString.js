c9ca80a999e97444f9e2e7c8882d5c45d554c451
ulm <125765792+UlmM35@users.noreply.github.com.>
2025-02-09 13:59:53 +0200

    Completed 6/7 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const splitString = string.split("").reverse().join("");
 
+    console.log(splitString);
+
+    return splitString;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  const converted = Math.round(number - 32) * 5/9;
+
+  const rounded = Math.round(converted * 10)/10;
+
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  const converted = (number * 9/5 + 32);
+
+  const rounded = Math.round(converted * 10)/10;
+
+  return rounded;
 };
 
 // Do not edit below this line
