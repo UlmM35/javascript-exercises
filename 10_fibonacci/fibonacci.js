4c55a73cdbd755a5d1bc8e50a9cc1d8699e8bfa9
UlmM35 <125765792+UlmM35@users.noreply.github.com>
2025-02-24 22:19:52 +0200

    Completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    let firstFib = 1;
+    let secondFib = 1;
+    let fibN = 0;
+    for (let i = 0; i < n - 2; i++) {
+        fibN = firstFib + secondFib;
+        firstFib = secondFib;
+        secondFib = fibN;
+    }
+    return fibN;
 };
 
 // Do not edit below this line
