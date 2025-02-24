89d43e5560fa942c3d88c8510ae7f394ca8c0ca0
UlmM35 <125765792+UlmM35@users.noreply.github.com>
2025-02-24 18:11:50 +0200

    Completed eight exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let sum = 1;
+  for (let i = 1; i <= n; i++) {
+    sum = sum * i; 
+  }
+  return sum;
 };
 
 // Do not edit below this line
