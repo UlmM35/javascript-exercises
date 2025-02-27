ca9e7e74e2942598aa8354526f17092e981f4b68
UlmM35 <125765792+UlmM35@users.noreply.github.com>
2025-02-27 22:46:09 +0200

    Completed 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    array.sort(function(a, b) {
+        let first = a.yearOfDeath - a.yearOfBirth;
+        let second = b.yearOfDeath - b.yearOfBirth;
+        if (a.yearOfDeath == undefined) {
+            first = new Date().getFullYear() - a.yearOfBirth;
+        } else if (b.yearOfDeath == undefined) {
+            second = new Date().getFullYear() - b.yearOfBirth;
+        }
+        return second - first;
+    });
+    return array[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
