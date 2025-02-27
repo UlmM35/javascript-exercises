c8d587333c4b0e9f92a36b82caaf68269b54e63f
UlmM35 <125765792+UlmM35@users.noreply.github.com>
2025-02-27 23:07:53 +0200

    Completed ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+ const palindromes = function (string) {
+    const stringWithOutComas = string.replaceAll(",", "");
+    const stringWithoutSpaces = stringWithOutComas.replaceAll(" ", "");
+    const stringWithoutUpper = stringWithoutSpaces.toLowerCase();
+    const stringWithoutStop = stringWithoutUpper.replaceAll(".", "");
+    const stringWithoutExc = stringWithoutStop.replaceAll("!", "");
+    const array = stringWithoutExc.split("");
+    array.reverse()
+    const joined = array.join(""); 
+    return stringWithoutExc == joined;
 };
 
 // Do not edit below this line
